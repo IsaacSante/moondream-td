ecc98384bbb68bdafa39e843ad928d5248895c68
Isaac Sante <43215955+IsaacSante@users.noreply.github.com>
2025-06-14 20:06:57 -0400

    moondream via transfomers

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-import moondream as md
+from transformers import AutoModelForCausalLM
 from PIL import Image
-# Initialize for Moondream Local Server
-model = md.vl(endpoint="http://localhost:2020/v1")
-# Load an image
-image = Image.open("test.jpg")
-# Ask a question
-answer = model.query(image, "What's in this image?")["answer"]
-print("Answer:", answer)
 
-# Stream the response
-# for chunk in model.caption(image, stream=True)["caption"]:
-#     print(chunk, end="", flush=True)
+model = AutoModelForCausalLM.from_pretrained(
+    "vikhyatk/moondream2",
+    trust_remote_code=True,
+    device_map={"": "mps"}       # Apple-GPU
+)
+
+image = Image.open("test.jpg")
+
+result = model.caption(image, length="short")["caption"]
+print(result)
