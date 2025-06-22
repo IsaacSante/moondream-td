a53b44fb284c42db6ce2762bee9d2b771d4b9a5d
Isaac Sante <43215955+IsaacSante@users.noreply.github.com>
2025-06-22 19:45:46 -0400

    Update prompt.py

--- utils/prompt.py
+++ utils/prompt.py
@@ -7,4 +7,4 @@ class Prompt:
     @property
     def text(self) -> str:
         objects_part = " or ".join(self._objects)
-        return f"Is the hand is holding something? If not, respond with 'none'. If the ahnd is grabbing any of the objects, a {objects_part}, then respond with the object name."
+        return f"Is a person grabbing of these objects {objects_part}? If they are then say which object. If they are not then say 'none.' Important: Only say the object if the persons hand is grabbing the object. It must be grabbing the object. If it in frame and not grabbing the object then say 'none.' "
